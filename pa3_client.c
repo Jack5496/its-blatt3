ef2da4ff21dfac1e465f69115391d4f2e5ff471f
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-12-04 23:40:41 +0100

    Update pa3_client.c

--- pa3_client.c
+++ pa3_client.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv){
      
      
      
-     int clientSocket nBytes;
+     int clientSocket, nBytes;
      
      struct sockaddr_in serverAddr;
      socklen_t addr_size;
