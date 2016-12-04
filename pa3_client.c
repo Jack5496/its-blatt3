bf02a0e372873dc03d1a5e7325e2be87a5844670
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-12-05 00:44:28 +0100

    Update pa3_client.c

--- pa3_client.c
+++ pa3_client.c
@@ -19,25 +19,162 @@ int debug = 1;
 char private_key[65536];
 
 
-#define KEYRING_DIR "/var/tmp/mycrypto"
-
-#define SENTENCE "I swear it is true"
-
-#define MAXLEN 4096
 
 
-#define fail_if_err(err)					\
-  do								\
-    {								\
-      if (err)							\
-        {							\
-          fprintf (stderr, "%s:%d: %s: %s\n",			\
-                   __FILE__, __LINE__, gpgme_strsource (err),	\
-		   gpgme_strerror (err));			\
-          exit (1);						\
-        }							\
-    }								\
-  while (0)
+
+
+
+
+
+
+#define fail_if_err(err)                                    \
+    do {                                                    \
+        if (err) {                                          \
+            fprintf (stderr, "%s:%d: %s: %s\n",             \
+                __FILE__, __LINE__, gpgme_strsource (err),  \
+                gpgme_strerror (err));                      \
+            exit (1);                                       \
+        }                                                   \
+    }                                                       \
+    while (0)
+
+void gpgSign(const char *keyID, const char *textToSign) {
+    gpgme_ctx_t ctx;
+    gpgme_error_t err;
+    gpgme_data_t in, out, result;
+    gpgme_verify_result_t verify_result;
+    gpgme_signature_t sig;
+    int tnsigs, nsigs;
+    int BUF_SIZE = 512;
+    char buf[BUF_SIZE + 1];
+    int ret;
+    /* Set the GPGME signature mode
+        GPGME_SIG_MODE_NORMAL : Signature with data
+        GPGME_SIG_MODE_CLEAR  : Clear signed text
+        GPGME_SIG_MODE_DETACH : Detached signature */
+    gpgme_sig_mode_t sigMode = GPGME_SIG_MODE_CLEAR;
+
+    /* Begin setup of GPGME */
+    gpgme_check_version (NULL);
+    setlocale (LC_ALL, "");
+    gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
+#ifndef HAVE_W32_SYSTEM
+    gpgme_set_locale (NULL, LC_MESSAGES, setlocale (LC_MESSAGES, NULL));
+#endif
+    /* End setup of GPGME */
+
+    err = gpgme_engine_check_version (GPGME_PROTOCOL_GPGCONF);
+    fail_if_err (err);
+    
+    // Create the GPGME Context
+    err = gpgme_new (&ctx);
+    // Error handling
+    fail_if_err (err);
+
+    // Set the context to textmode
+    gpgme_set_textmode (ctx, 1);
+    // Enable ASCII armor on the context
+    gpgme_set_armor (ctx, 1);
+
+    unsigned int textLength = strlen (textToSign);
+
+    // Create a data object that contains the text to sign
+    err = gpgme_data_new_from_mem (&in, textToSign, textLength, 0);
+    // Error handling
+    fail_if_err (err);
+
+    // Create a data object pointing to the out buffer
+    err = gpgme_data_new (&out);
+    // Error handling
+    fail_if_err (err);
+
+    // Create a data object pointing to the result buffer
+    err = gpgme_data_new (&result);
+    // Error handling
+    fail_if_err (err);
+
+    // Sign the contents of "in" using the defined mode and place it into "out"
+    err = gpgme_op_sign (ctx, in, out, sigMode);
+    // Error handling
+    fail_if_err (err);
+
+    // Rewind the "out" data object
+    ret = gpgme_data_seek (out, 0, SEEK_SET);
+    // Error handling
+    if (ret)
+        fail_if_err (gpgme_err_code_from_errno (errno));
+
+    // Read the contents of "out" and place it into buf
+    while ((ret = gpgme_data_read (out, buf, BUF_SIZE)) > 0) {
+        // Write the contents of "buf" to the console
+        fwrite (buf, ret, 1, stdout);
+    }
+
+    fwrite ("\n", 1, 1, stdout);
+
+    // Error handling
+    if (ret < 0)
+        fail_if_err (gpgme_err_code_from_errno (errno));
+
+    // Rewind the "out" data object
+    ret = gpgme_data_seek (out, 0, SEEK_SET);
+
+    // Perform a decrypt/verify action
+    err = gpgme_op_decrypt_verify (ctx, out, result);
+
+    // Retrieve the verification result
+    verify_result = gpgme_op_verify_result (ctx);
+
+    // Error handling
+    if (err != GPG_ERR_NO_ERROR && !verify_result)
+        fail_if_err (err);
+
+    // Check if the verify_result object has signatures
+    if (verify_result && verify_result->signatures) {
+        // Iterate through the signatures in the verify_result object
+        for (nsigs=0, sig=verify_result->signatures; sig; sig = sig->next, nsigs++) {
+            fprintf(stdout, "Signature made with Key: %s\n", sig->fpr);
+            fprintf(stdout, "Created: %lu; Expires %lu\n", sig->timestamp, sig->exp_timestamp);
+            char *validity = sig->validity == GPGME_VALIDITY_UNKNOWN? "unknown":
+                    sig->validity == GPGME_VALIDITY_UNDEFINED? "undefined":
+                    sig->validity == GPGME_VALIDITY_NEVER? "never":
+                    sig->validity == GPGME_VALIDITY_MARGINAL? "marginal":
+                    sig->validity == GPGME_VALIDITY_FULL? "full":
+                    sig->validity == GPGME_VALIDITY_ULTIMATE? "ultimate": "[?]";
+            char *sig_status = gpg_err_code (sig->status) == GPG_ERR_NO_ERROR? "GOOD":
+                    gpg_err_code (sig->status) == GPG_ERR_BAD_SIGNATURE? "BAD_SIG":
+                    gpg_err_code (sig->status) == GPG_ERR_NO_PUBKEY? "NO_PUBKEY":
+                    gpg_err_code (sig->status) == GPG_ERR_NO_DATA? "NO_SIGNATURE":
+                    gpg_err_code (sig->status) == GPG_ERR_SIG_EXPIRED? "GOOD_EXPSIG":
+                    gpg_err_code (sig->status) == GPG_ERR_KEY_EXPIRED? "GOOD_EXPKEY": "INVALID";
+            fprintf(stdout, "Validity: %s; Signature Status: %s", validity, sig_status);
+            fwrite("\n", 1, 1, stdout);
+            tnsigs++;
+        }
+    }
+
+    if (err != GPG_ERR_NO_ERROR && tnsigs < 1)
+        fail_if_err(err);
+
+    // Release the "in" data object
+    gpgme_data_release (in);
+    // Release the "out" data object
+    gpgme_data_release (out);
+
+    // Release the context
+    gpgme_release (ctx);
+}
+
+
+
+
+
+
+
+
+
+
+
 
  
 int main(int argc, char **argv){
@@ -83,103 +220,6 @@ int main(int argc, char **argv){
 
      /*Send message to server*/
      sendto(clientSocket,message,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-  gpgme_error_t error;
-  gpgme_engine_info_t info;
-  gpgme_ctx_t context;
-  gpgme_key_t recipients[2] = {NULL, NULL};
-  gpgme_data_t clear_text, encrypted_text;
-  gpgme_encrypt_result_t  result;
-  gpgme_user_id_t user;
-  char *buffer;
-  ssize_t nbytes;
-
-  /* Initializes gpgme */
-  gpgme_check_version (NULL);
-  
-  /* Initialize the locale environment.  */
-  setlocale (LC_ALL, "");
-  gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
-#ifdef LC_MESSAGES
-  gpgme_set_locale (NULL, LC_MESSAGES, setlocale (LC_MESSAGES, NULL));
-#endif
-
-  error = gpgme_new(&context);
-  fail_if_err(error);
-  /* Setting the output type must be done at the beginning */
-  gpgme_set_armor(context, 1);
-
-  /* Check OpenPGP */
-  error = gpgme_engine_check_version(GPGME_PROTOCOL_OpenPGP);
-  fail_if_err(error);
-  error = gpgme_get_engine_info (&info);
-  fail_if_err(error);
-  while (info && info->protocol != gpgme_get_protocol (context)) {
-    info = info->next;
-  }  
-  /* TODO: we should test there *is* a suitable protocol */
-  fprintf (stderr, "Engine OpenPGP %s is installed at %s\n", info->version,
-	   info->file_name); /* And not "path" as the documentation says */
-
-  /* Initializes the context */
-  error = gpgme_ctx_set_engine_info (context, GPGME_PROTOCOL_OpenPGP, NULL,
-				     KEYRING_DIR);
-  fail_if_err(error);
-
-  error = gpgme_op_keylist_start(context, "John Smith", 1);
-  fail_if_err(error);
-  error = gpgme_op_keylist_next(context, &recipients[0]);
-  fail_if_err(error);
-  error = gpgme_op_keylist_end(context);
-  fail_if_err(error);
-
-  user = recipients[0]->uids;
-  printf("Encrypting for %s <%s>\n", user->name, user->email);
-
-  /* Prepare the data buffers */
-  error = gpgme_data_new_from_mem(&clear_text, SENTENCE, strlen(SENTENCE), 1);
-  fail_if_err(error);
-  error = gpgme_data_new(&encrypted_text);
-  fail_if_err(error); 
-
-  /* Encrypt */
-  error = gpgme_op_encrypt(context, recipients, 
-			   GPGME_ENCRYPT_ALWAYS_TRUST, clear_text, encrypted_text);
-  fail_if_err(error);
-  result = gpgme_op_encrypt_result(context);
-  if (result->invalid_recipients)
-    {
-      fprintf (stderr, "Invalid recipient found: %s\n",
-	       result->invalid_recipients->fpr);
-      exit (1);
-    }
-
-  nbytes = gpgme_data_seek (encrypted_text, 0, SEEK_SET);
-  if (nbytes == -1) {
-    fprintf (stderr, "%s:%d: Error in data seek: ",			
-	     __FILE__, __LINE__);
-    perror("");
-    exit (1);					
-    }  
-  buffer = malloc(MAXLEN);
-  nbytes = gpgme_data_read(encrypted_text, buffer, MAXLEN);
-  if (nbytes == -1) {
-    fprintf (stderr, "%s:%d: %s\n",			
-	     __FILE__, __LINE__, "Error in data read");
-    exit (1);					
-  }
-  buffer[nbytes] = '\0';
-  printf("Encrypted text (%i bytes):\n%s\n", (int)nbytes, buffer);
-  /* OK */
      
      
      
