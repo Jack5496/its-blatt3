af37dfe4b8dc9f67202b3662f50a4233710b11dd
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-12-04 23:02:04 +0100

    Update pa3_server.c

--- pa3_server.c
+++ pa3_server.c
@@ -57,7 +57,7 @@ int main(int argc, char **argv){
 	    
 	    struct sockaddr_in si_me, si_other;
      
-    int s, i, slen = sizeof(si_other) , recv_len;
+    int s, slen = sizeof(si_other) , recv_len;
 	    
     //create a UDP socket
     if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
