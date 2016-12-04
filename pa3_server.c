0730c4b953156949b5cca107906fe489cfc89e4c
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-12-04 23:01:09 +0100

    Update pa3_server.c

--- pa3_server.c
+++ pa3_server.c
@@ -3,9 +3,8 @@
 #include <signal.h> //Damit ich Signale abfangen kann
 #include <stdio.h>
 
-#include <errno.h>
-#include <sys/socket.h>
-#include <resolv.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
 
 int server_port = 80;
 int debug = 1;
@@ -27,6 +26,12 @@ void last_wish(int s){
            exit(1); //schließe
 }
 
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
+
 int main(int argc, char **argv){
    //Handlet aktivierung für STRG+C
    //http://stackoverflow.com/questions/1641182/how-can-i-catch-a-ctrl-c-event-c
@@ -44,35 +49,54 @@ int main(int argc, char **argv){
     needed_arguments++; //Server Port
  
     if(argc==needed_arguments){       
-           // Einfacher UDP Server https://www.cs.utah.edu/~swalton/listings/sockets/programs/part1/chap4/udp-server.c    
-           
-        int sd;
-	struct sockaddr_in addr;
-        
         server_port = atoi(argv[1]);
-	sd = socket(PF_INET, SOCK_DGRAM, 0);
-	bzero(&addr, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(server_port);
-	addr.sin_addr.s_addr = INADDR_ANY;
-	if ( bind(sd, (struct sockaddr*)&addr, sizeof(addr)) != 0 ){
-		perror("bind");
-    	}
+	
 	    
-	if(debug){
-        	printf("Starting on Port: %s\n", server_port);
+	    
+	    
+	    
+	    struct sockaddr_in si_me, si_other;
+     
+    int s, i, slen = sizeof(si_other) , recv_len;
+	    
+    //create a UDP socket
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        die("socket");
+    }
+     
+    // zero out the structure
+    memset((char *) &si_me, 0, sizeof(si_me));
+     
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(server_port);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+     
+    //bind socket to port
+    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
+    {
+        die("bind");
+    }
+     
+    //keep listening for data
+    while(listen)
+    {
+        printf("Waiting for data...");
+        fflush(stdout);
+         
+        //try to receive some data, this is a blocking call
+        if ((recv_len = recvfrom(s, buffer, 65536, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        {
+            die("recvfrom()");
         }
-	    
-	while (listen)
-	{	
-		int bytes, addr_len=sizeof(addr);
-
-		bytes = recvfrom(sd, buffer, sizeof(buffer), 0, (struct sockaddr*)&addr, &addr_len);
-		printf("msg from %s:%d (%d bytes)\n", inet_ntoa(addr.sin_addr),
-						ntohs(addr.sin_port), bytes);
-		
-            
-	}
+         
+        //print details of the client/peer and the data received
+        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        printf("Data: %s\n" , buffer);
+        
+    }
+ 
+    close(s);
         
        
                 
