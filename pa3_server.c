a8d3f218b8ce16cb1c4edc309bed5ffbc65eb672
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-12-06 18:30:28 +0100

    Update pa3_server.c

--- pa3_server.c
+++ pa3_server.c
@@ -25,11 +25,13 @@ gpgme_data_t in;  /* Data Input */
 gpgme_data_t plain;  /* Plaintext of Message */
 gpgme_verify_result_t verify_result;  /* The verify of the Signed Message */
 gpgme_signature_t sig;  /* The signature */
-int tnsigs, nsigs;
 
+/**
+* Hiermit wird unser GPG initialisiert
+*/
 void gpgInit(){
 	/* Begin setup of GPGME */
-	gpgme_check_version (NULL);
+	gpgme_check_version (NULL); /* Entnommen aus der Doku */
 	setlocale (LC_ALL, "");
 	gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
 	/* End setup of GPGME */
@@ -41,8 +43,7 @@ void gpgInit(){
 		return;
 	}
 
-	// Create the GPGME Context
-	err = gpgme_new (&ctx);
+	err = gpgme_new (&ctx);  /* Erstellen des neuen Context */
 	if(err){
 		printf("GPGME: Context creation failed\n");
 		keep_alive = 0;
@@ -50,11 +51,10 @@ void gpgInit(){
 	}
 }
 
+/**
+* Hierdurch beenden wir unser GPG
+*/
 void gpgRelease(){
-	// Release the "in" data object
-	gpgme_data_release (in);
-
-	// Release the context
 	gpgme_release (ctx);
 }
 
@@ -63,11 +63,11 @@ void gpgRelease(){
 */
 void last_wish(int i){
 	printf("\nManuelles Beenden\n");
-	keep_alive=0;
+	keep_alive=0; /* Wir möchten unsere While Loop beenden */
 }
 
 void gpgCheckSign() {
-	// Create a data object that contains the text to sign
+	 /* Erstelle Data in, welches unseren text beinhaltet */
 	err = gpgme_data_new_from_mem (&in, buffer, data_length, 1);
 	if(err){
 		printf("GPGME: Read Buffer into In failed\n");
@@ -75,43 +75,72 @@ void gpgCheckSign() {
 		return;
 	}
 
+	 /* Erstelle Data Plain, welches den Plaintext enthalten soll */
 	err = gpgme_data_new (&plain);
 	if(err){
 		printf("GPGME: Data new plain failed\n");
 		keep_alive = 0;
+		gpgme_data_release (in);
 		return;
 	}
 	
-	// Perform a verify action
+	 /* Führe ein Verify aus */
 	err = gpgme_op_verify (ctx, in,NULL,plain);
 	if(err){
 		printf("GPGME: Verify failed\n");
 		gpgme_data_release (plain);
+		gpgme_data_release (in);
 		keep_alive = 0;
 		return;
 	}
 	
-	// Retrieve the verification result
+	 /* Hole das Ergebniss des Verify */
 	verify_result = gpgme_op_verify_result (ctx);
 
+	 /* Fahre fort, nur wenn wir was haben und dies Signaturen besitzt */
 	if (verify_result && verify_result->signatures){
+		 /* Fahre fort, nur wenn wir bei dem Status keinen Fehler haben */
 		if(gpg_err_code(verify_result->signatures->status)==GPG_ERR_NO_ERROR){
-			gpgme_key_t key;
+			gpgme_key_t key;  /* Erstelle neuen halter unseres Keys */
+			
+			 /* Fülle nun den Key */
 			err = gpgme_get_key (ctx, verify_result->signatures->fpr, &key, 0);
 			if(err){
 				printf("GPGME: get Key failed\n");
 				gpgme_data_release (plain);
+				gpgme_data_release (in);
 				keep_alive = 0;
 				return;
 			}
 
-			printf("%s: ",key->uids->name);
-
+			 /* AUSGABE: Namen des Senders falls vorhanden */
+			if(key->uids->name){
+				printf("%s: ",key->uids->name);
+			}
+			else{
+				printf("GPGME: No Name in Key UIDS\n");
+				gpgme_data_release (plain);
+				gpgme_data_release (in);
+				keep_alive = 0;
+				return;
+			}
+				
+			/* Erstelle halter der Länge des Plaintext */
 			size_t plainTextLength;
 			
-			gpgme_data_seek(plain,0,SEEK_SET);
+			err = gpgme_data_seek(plain,0,SEEK_SET); /* Setzte Pointer zum anfang*/
+			if(err){
+				printf("GPGME: Data Seek Failed\n");
+				gpgme_data_release (plain);
+				gpgme_data_release (in);
+				keep_alive = 0;
+				return;
+			}
+			
+			/* Hole den Plaintext */
 			char* plainText = gpgme_data_release_and_get_mem(plain,&plainTextLength);
 
+			/* Printe jeden einzelnen Char hinter den Namen */
 			int i;
 			for(i=0; i<plainTextLength; i++){
 				printf("%c",plainText[i]);	
@@ -126,9 +155,12 @@ void gpgCheckSign() {
 	else{
 		printf("GPGME: Verify result failed\n");
 		gpgme_data_release (plain);
+		gpgme_data_release (in);
 		keep_alive = 0;
 		return;
 	}
+	
+	gpgme_data_release (in);
 	
 }
 
@@ -147,15 +179,18 @@ int i;
    // Ende für STRG+C
     
     
-    int needed_arguments = 1; //programm self
+    int needed_arguments = 1; //programm selber
     needed_arguments++; //Server Port
 	
+    /* Starte GPGME */ 
     gpgInit();
  
+	/* Prüfe ob genug Argumente vorhanden */
     if(argc==needed_arguments){       
 	int err_port = 0;
         i=0;
         while(argv[1][i] != '\0'){
+		/* Prüfe ob der Port nur aus Zahlen besteht */
              if (argv[1][i] < 47 || argv[1][i] > 57){
                 err_port = 1;
                 break;
@@ -164,18 +199,19 @@ int i;
         }
         if(err_port==1){
             printf("Error: No valid Port\n");
-            keep_alive = 0;
+            gpgRelease();
+	    return;
         }        
         server_port = atoi(argv[1]);
 	    
 	//Einfacher UDP Server http://www.programminglogic.com/sockets-programming-in-c-using-udp-datagrams/
 	
-	
+	/* Erstelle Alles für das aufsetzen des Sockets */
 	struct sockaddr_in serverAddr;
 	struct sockaddr serverStorage;
 	socklen_t addr_size;
 
-	/*Create UDP socket*/
+	/* Create UDP socket */
 	udpSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if(udpSocket<0){
 	    printf("Unable to setup Socket\n");	
@@ -198,17 +234,19 @@ int i;
 	  /*Initialize size variable to be used later on*/
 	  addr_size = sizeof serverStorage;
 	    
+	  /* Erstelle genug Platz für ankommende Nachrichten */
 	  buffer = malloc(sizeof(char)*65536);
 	    
 	  if(keep_alive){
 		printf("Server starting: %s:%i \n\n",server_adress,server_port);  	  
 	  }
+	  /* Solange wir aktiv sein sollen */
 	  while(keep_alive){
 		  
-	    /* Try to receive any incoming UDP datagram. Address and port of 
-	      requesting client will be stored on serverStorage variable */
+	    /* Hole alle ankommenden Daten und speichere diese in Buffer zwischen */
 	    data_length = recvfrom(udpSocket,buffer,65536,0,(struct sockaddr *)&serverStorage, &addr_size);
 	    
+            /* Falls wir was erhalten haben */
 	    if(data_length>=0){	  
 		    gpgCheckSign();
 	    }
@@ -220,9 +258,11 @@ int i;
 	    }
 	  }
 	    
+	    /* Falls wir unseren Socket noch schließen müssen, tun wir das */
 	  if(udpSocket > 0){
 			close(udpSocket);
 	  }
+	  /* Wir freen mal alles was noch offen ist */
 	  free(buffer);
 	  gpgRelease();
        
