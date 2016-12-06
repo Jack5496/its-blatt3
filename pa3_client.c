a16584e0bc00d6dd4da4a79be724fdc9b56fcad5
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-12-06 13:35:00 +0100

    Update pa3_client.c

--- pa3_client.c
+++ pa3_client.c
@@ -14,7 +14,6 @@ char server_adress[65536];
 int server_port = 80;
 char username[65536];
 char message[65536];
-char signature[65536];
 int debug = 1;
 
 char private_key[65536];
@@ -42,11 +41,12 @@ char private_key[65536];
 void signText(){
     gpgme_ctx_t ctx;
     gpgme_error_t err;
-    gpgme_data_t in, out, result;
+     gpgme_data_t in, out, result;
     gpgme_verify_result_t verify_result;
     gpgme_signature_t sig;
     int tnsigs, nsigs;
-    
+    int BUF_SIZE = 512;
+    char buf[BUF_SIZE + 1];
     int ret;
     /* Set the GPGME signature mode
         GPGME_SIG_MODE_NORMAL : Signature with data
@@ -80,69 +80,14 @@ void signText(){
 
     // Sign the contents of "in" using the defined mode and place it into "out"
     fail_if_err (gpgme_op_sign (ctx, in, out, sigMode));
+
+    size_t signature_length = 0;
     
-    // Rewind the "out" data object
-    ret = gpgme_data_seek (out, 0, SEEK_SET);
-    // Error handling
-    if (ret)
-        fail_if_err (gpgme_err_code_from_errno (errno));
-
-   
-    // Read the contents of "out" and place it into buf
-    ret = gpgme_data_read (out, signature, 65536);
-    printf("######### START ######\n\n"); 
-    printf("%s\n",signature);
-    printf("######### END ######\n\n"); 
+    signature = gpgme_release_and_get_mem(out,&signature_length);
     
-    // Error handling
-    if (ret < 0)
-        fail_if_err (gpgme_err_code_from_errno (errno));
-
-    // Rewind the "out" data object
-    ret = gpgme_data_seek (out, 0, SEEK_SET);
-
-    // Perform a decrypt/verify action
-    err = gpgme_op_decrypt_verify (ctx, out, result);
-
-    // Retrieve the verification result
-    verify_result = gpgme_op_verify_result (ctx);
-
-    // Error handling
-    if (err != GPG_ERR_NO_ERROR && !verify_result)
-        fail_if_err (err);
-
-    // Check if the verify_result object has signatures
-    if (verify_result && verify_result->signatures) {
-        // Iterate through the signatures in the verify_result object
-        for (nsigs=0, sig=verify_result->signatures; sig; sig = sig->next, nsigs++) {
-            fprintf(stdout, "Signature made with Key: %s\n", sig->fpr);
-            fprintf(stdout, "Created: %lu; Expires %lu\n", sig->timestamp, sig->exp_timestamp);
-            char *validity = sig->validity == GPGME_VALIDITY_UNKNOWN? "unknown":
-                    sig->validity == GPGME_VALIDITY_UNDEFINED? "undefined":
-                    sig->validity == GPGME_VALIDITY_NEVER? "never":
-                    sig->validity == GPGME_VALIDITY_MARGINAL? "marginal":
-                    sig->validity == GPGME_VALIDITY_FULL? "full":
-                    sig->validity == GPGME_VALIDITY_ULTIMATE? "ultimate": "[?]";
-            char *sig_status = gpg_err_code (sig->status) == GPG_ERR_NO_ERROR? "GOOD":
-                    gpg_err_code (sig->status) == GPG_ERR_BAD_SIGNATURE? "BAD_SIG":
-                    gpg_err_code (sig->status) == GPG_ERR_NO_PUBKEY? "NO_PUBKEY":
-                    gpg_err_code (sig->status) == GPG_ERR_NO_DATA? "NO_SIGNATURE":
-                    gpg_err_code (sig->status) == GPG_ERR_SIG_EXPIRED? "GOOD_EXPSIG":
-                    gpg_err_code (sig->status) == GPG_ERR_KEY_EXPIRED? "GOOD_EXPKEY": "INVALID";
-            fprintf(stdout, "Validity: %s; Signature Status: %s", validity, sig_status);
-            fwrite("\n", 1, 1, stdout);
-            tnsigs++;
-        }
-    }
-
-    if (err != GPG_ERR_NO_ERROR && tnsigs < 1)
-        fail_if_err(err);
-
+    
     // Release the "in" data object
     gpgme_data_release (in);
-    // Release the "out" data object
-    gpgme_data_release (out);
-
     // Release the context
     gpgme_release (ctx);
 }
@@ -193,12 +138,15 @@ int main(int argc, char **argv){
 
      nBytes = strlen(message) + 1;
 
+     char* signature = malloc(sizeof(char)*65536);
      /*Sign message*/
      signText();
      
      /*Send message to server*/
      sendto(clientSocket,message,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
-             
+        
+     free(signature);
+        
     }
     else{
         printf("usage: ./pa3_client SERVER_ADRESS SERVER_PORT USERNAME \"Message to encypt\" \n");
